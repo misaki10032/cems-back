58323783c528173792a5092dcbddabd5ee3bba62
blank <1069664381@qq.com>
2021-08-10 10:24:02 +0800

    2021-08-10 [cxy] 10:23 [新增AES加密工具类]

--- src/main/java/com/cems/util/ShiroMd5Util.java
+++ src/main/java/com/cems/util/ShiroMd5Util.java
@@ -1,8 +1,6 @@
 package com.cems.util;
-
 import org.apache.shiro.crypto.hash.SimpleHash;
 import org.apache.shiro.util.ByteSource;
-
 /**
  * @ClassName UserController
  * @Author 陈新予
@@ -20,7 +18,7 @@ public class ShiroMd5Util {
     public static String toPwdMd5(String username, String password) {
         String hashAlgorithmName = "MD5";//加密方式
         ByteSource salt = ByteSource.Util.bytes(username);//以账号作为盐值
-        int hashIterations = 1024;//加密1024次
+        int hashIterations = 1024;//哈希散列1024次
         return new SimpleHash(hashAlgorithmName, password, salt, hashIterations).toHex();
     }
 }
